1e1563c477988779da75e348f36c9f3d637a80a6
hannerco <hanner.condeo@unac.edu.co>
2026-04-07 19:44:28 -0500

    Method countryValidation created but not implemented in assessRisk

--- src/main/java/edu/unac/service/TravelRiskAssessmentService.java
+++ src/main/java/edu/unac/service/TravelRiskAssessmentService.java
@@ -1,10 +1,15 @@
 package edu.unac.service;
 
+import edu.unac.domain.country.Country;
+import edu.unac.domain.exception.ExternalServiceException;
+import edu.unac.domain.travel.RiskLevel;
 import edu.unac.domain.travel.TravelRequest;
 import edu.unac.domain.travel.TravelRiskResponse;
 import edu.unac.service.external.CountryClient;
 import edu.unac.service.external.HolidayClient;
 import edu.unac.service.external.WeatherClient;
+
+import java.util.List;
 
 public class TravelRiskAssessmentService {
     private final WeatherClient weatherClient;
@@ -20,4 +25,27 @@ public class TravelRiskAssessmentService {
     public TravelRiskResponse assessRisk(TravelRequest request) {
         return new TravelRiskResponse();
     }
+
+    public RiskLevel countryValidation(String countryCode, int travelerExperienceYears){
+        List<Country> countries = countryClient.getCountry(countryCode);
+
+        if (countries == null || countries.isEmpty()) {
+            throw new ExternalServiceException("No se encontró el pais");
+        }
+
+        Country country = countries.get(0);
+
+
+        if(country.getPopulation() > 100000000 && travelerExperienceYears < 2){
+            return RiskLevel.HIGH_RISK;
+
+        } else if (!country.getLanguages().containsKey("eng") || !country.getLanguages().containsKey("spa")){
+            return RiskLevel.MEDIUM_RISK;
+        } else {
+            return RiskLevel.SAFE;
+        }
+
+
+    }
+
 }
